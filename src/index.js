b3184373532957ce58a210d71dbe0cc0542ba913
mfpinheiro <mateusfpinheiro@gmail.com>
2017-12-09 10:39:38 -0200

    Just finished the refactoring of the search class.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-
-
 import { API_URL } from './config';
 import toJSON from './utils';
 import album from './album';
+import search from './search';
 
 export default class SpotifyWrapper {
   constructor(options) {
     this.apiURL = options.apiURL || API_URL;
     this.token = options.token;
     this.album = album.bind(this)();
+    this.search = search.bind(this)();
   }
   request(url) {
     const headers = {
